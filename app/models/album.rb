4271fb1eeceb35f4771524a744ac92c8716ba346
Student <example@example.com>
2020-01-13 14:40:39 -0800

    add test to validate album name and character length.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,5 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 100
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Album do
-it { should have_many(:songs) }
-  end
+  it { should have_many(:songs) }
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(100) }
+end
