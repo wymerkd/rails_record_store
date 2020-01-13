c89a039af5a1323a25def5bf76ab41d53d52d4a4
Student <example@example.com>
2020-01-13 15:30:47 -0800

    add passing tests for case

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -4,4 +4,8 @@ describe Album do
   it { should have_many(:songs) }
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
+  it("titleizes the name of an album") do
+    album = Album.create({name: "giant steps", genre: "jazz"})
+    expect(album.name()).to(eq("Giant Steps"))
+  end
 end
